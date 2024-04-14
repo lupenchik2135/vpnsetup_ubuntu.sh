a6dc54683a511b043c6bbcd986db652f26ad30b9
Арсений Токарев <99415550+lupenchik2135@users.noreply.github.com>
2024-04-14 15:44:32 +0300

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -262,7 +262,7 @@ install_vpn_pkgs() {
     set -x
     apt-get -yqq install libnss3-dev libnspr4-dev pkg-config \
       libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev \
-      libcurl4-nss-dev flex bison gcc make libnss3-tools \
+      flex bison gcc make libnss3-tools \
       libevent-dev libsystemd-dev uuid-runtime ppp xl2tpd >/dev/null
   ) || exiterr2
   if [ "$os_type" = "debian" ] && [ "$os_ver" = 12 ]; then
